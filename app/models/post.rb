41c6074d9b54b4ad17da10375fecc64e05b36057
sfespinosa <scottjames.espinosa@gmail.com>
2020-06-28 12:59:05 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,22 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories
 
+  def unique_commenting_users
+    users = []
+    self.comments.each do |comment|
+      users << comment.user
+    end
+    users = users.uniq
+  end
 
+  def categories_attributes=(category)
+    category.values.each do |name|
+      if name.values[0] != ""
+        category = Category.find_or_create_by(name)
+        self.categories << category
+      end
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
+Category.destroy_all
+User.destroy_all
+Post.destroy_all
+PostCategory.destroy_all
+Comment.destroy_all
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +10,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+cool = Category.create(name: "cool")
+tight = Category.create(name: "tight")
+
+scott = User.create(username: 'scottrick', email: 'koka922@aol.com')
+monique = User.create(username: 'monmon87', email: 'monmon87@aol.com')
+
+post1 = Post.create(title: "EDM Rules", content: "EDM is so cool")
+post2 = Post.create(title: "Marvel Rules", content: "Marvel is so cool")
+
+post_category1 = PostCategory.create(post: post1, category: cool)
+post_category2 = PostCategory.create(post: post2, category: tight)
+
+comment1 = Comment.create(content: "YUP!", user: scott, post: post1)
+comment2 = Comment.create(content: "Yes!", user: monique, post: post2)
